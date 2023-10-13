c6ae85d83f4adca12c2908aa6f4af9cb9591f184
FCRCH <fabiancastrilloramirez@gmail.com>
2023-10-12 21:03:37 -0600

    Semana 5 Toggle
    Se intento añadir Toggle

--- FCastrillo/src/app/app.module.ts
+++ FCastrillo/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
